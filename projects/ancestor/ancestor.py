712131cbacfc73421d541a83d39c1b49340a09b9
Ahmad Guenoun <amguenoun@gmail.com>
2020-04-14 18:15:43 -0400

    added dft to find earliest ancestor and all tests pass

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -14,3 +14,28 @@ def earliest_ancestor(ancestors, starting_node):
         # if so add connection
         else:
             graph.vertices[relationship[1]].add(relationship[0])
+
+    # if no parents then return -1
+    if starting_node not in graph.vertices:
+        return -1
+
+    # use DFT to find greatest distance
+    visited = set()
+    stack = Stack()
+    stack.push([starting_node])
+    max_path = []
+    while stack.size() != 0:
+        path = stack.pop()
+        if path[-1] not in visited:  # prevent cycles
+            visited.add(path[-1])
+            if len(path) > len(max_path):
+                max_path = path
+            # if more than one, return the one with smaller numeric id
+            elif len(path) == len(max_path):
+                if max_path[-1] > path[-1]:
+                    max_path = path
+            if path[-1] in graph.vertices:
+                for parent in graph.get_neighbors(path[-1]):
+                    stack.push(path + [parent])
+
+    return max_path[-1]
